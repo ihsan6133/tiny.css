58f3e396fb9c5b9d056744b0e468534d11884330
Ihsan <mihsan6133@gmail.com>
2023-08-12 17:28:41 +0400

    Support for exclusively light/dark themes, and darker color scheme for dark mode

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,10 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var postcssImport = require('postcss-import');
 var cssnano = require('cssnano');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function() {
-    return gulp.src('src/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([
-            postcssImport(),
-            cssnano()
-        ]))
-        .pipe(sourcemaps.write('.'))
+    return gulp.src('src/builds/*.css')
+        .pipe(postcss([postcssImport(), cssnano()]))
         .pipe(gulp.dest('dist'));
 });
 
