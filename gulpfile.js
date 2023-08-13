29b25fe6ad7550889e13045bb63b262eceb08377
Ihsan <mihsan6133@gmail.com>
2023-08-13 11:34:06 +0400

    Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var postcssImport = require('postcss-import');
-var cssnano = require('cssnano');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const postcssImport = require('postcss-import');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
     return gulp.src('src/builds/*.css')
-        .pipe(postcss([postcssImport(), cssnano()]))
+        .pipe(postcss([postcssImport(), autoprefixer(), cssnano()]))
         .pipe(gulp.dest('dist'));
 });
 
